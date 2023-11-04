36f006dfdde6f292db9e64b7b0ed46f9e33c4a85
Paige <paigehcarroll@gmail.com>
2023-11-04 17:00:26 -0500

    added navbar list items, removed boilerplate code from app.jsx, installed reacr-router-dom and imported it to main.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,26 @@
 function NavBar() {
-    return (
-        <a>This is the NavBar Component!</a>
-    )
+  return (
+    <div>
+      <a>This is the NavBar Component!</a>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a>Home</a>
+        </li>
+        <li className="nav-item">
+          <a>About Me</a>
+        </li>
+        <li className="nav-item">
+          <a>Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a>Contact</a>
+        </li>
+        <li className="nav-item">
+          <a>Resume</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
 import App from './App.jsx'
+
+
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
