6e2633c9ed8dd2c22c7768be97342369702d8d79
Paige <paigehcarroll@gmail.com>
2023-11-05 17:29:16 -0600

    added form to contact component and some styling, added structure and content to portfolio page-need to figure out mapping

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,104 @@
+import { useState } from "react";
+
 export default function Contact() {
-    return (
-        <div>
-            <h1>Contact</h1>
-            <p>how to contact me</p>
+  // Create state variables for the fields in the form
+  // Also set their initial values to an empty string
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Get the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either name, email, and message
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    //first validate email and check if name is empty, if so set an error message to be displayed on the page
+    if (!validateEmail(email)) {
+      setErrorMessage("Email is invalid.");
+      return;
+    }
+    if (!name) {
+      setErrorMessage("Please provide a name.");
+    }
+
+    // If successful, we want to clear out the input after registration.
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <div className="container">
+        <div className="row">
+          <div>
+            <form className="form" onSubmit={handleFormSubmit}>
+              <div>
+                <label>Name:</label>
+                <input
+                  value={name}
+                  type="text"
+                  id="name"
+                  onChange={handleInputChange}
+                  placeholder="Your Name"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div>
+                <label>Email:</label>
+                <input
+                  value={email}
+                  type="email"
+                  id="email"
+                  onChange={handleInputChange}
+                  placeholder="Your Email"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div>
+                <label>Message:</label>
+                <input
+                  value={message}
+                  type="text"
+                  id="message"
+                  onChange={handleInputChange}
+                  placeholder="Message"
+                  required
+                  className="form-control"
+                />
+              </div>
+              <div className="text-center m-2">
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  onClick={handleFormSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,98 @@
+// function Project({ title, description, githubLink, deployedLink }) {
+//     return (
+//         <div className="container">
+//         <div class="card">
+//           <img class="card-img-top" alt="" />
+//           <div class="card-body">
+//             <h5 class="card-title">{title}</h5>
+//             <p class="card-text">
+//               {description}
+//             </p>
+//           </div>
+//           <div class="card-body">
+//             <a href={`${githubLink}`}class="card-link">
+//               GitHub Repository
+//             </a>
+//             <a href={`${deployedLink}`} class="card-link">
+//               Deployed Site
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     )
+// }
+
 export default function Portfolio() {
-    return (
-        <div>
-            <h1>Portfolio</h1>
-            <p>projects i have made</p>
+  const projects = [
+    {
+      title: "bookquesters",
+      description:
+        "This web application allows users to search for book titles sourced from the Google Books API and save them to a list that is saved in local storage.",
+      githubLink: "https://github.com/sillytsundere/bookquesters",
+      deployedLink: "https://sillytsundere.github.io/bookquesters/",
+    },
+    {
+      title: "eva - hangman",
+      description:
+        "A hangman game made using vanilla JavaScript and HTML styled with BootstrapCSS. Themed after a 90's anime.",
+      githubLink: "https://github.com/sillytsundere/hangman-game",
+      deployedLink: "N/A",
+    },
+    {
+      title: "cosfolio",
+      description:
+        "A forum where users can save and share cosplay creations as well as view other users' creations.",
+      githubLink: "https://github.com/sillytsundere/cosfolio",
+      deployedLink: "https://cosfolio-cbc30e9a9851.herokuapp.com/",
+    },
+    {
+      title: "comp - sci - blog",
+      description:
+        "This web application is a CMS-style blog that allows users to create an account, log in, share posts and view and comment on other user's posts.",
+      githubLink: "https://github.com/sillytsundere/comp-sci-blog",
+      deployedLink: "https://comp-tech-talk-1c5888367981.herokuapp.com/",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      {/* {projects.map((project) => {
+        <div className="container">
+          <section class="card">
+            <img class="card-img-top" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">{project.title}</h5>
+              <p class="card-text">{project.description}</p>
+            </div>
+            <div class="card-body">
+              <a href={`${project.githubLink}`} class="card-link">
+                GitHub Repository
+              </a>
+              <a href={`${project.deployedLink}`} class="card-link">
+                Deployed Site
+              </a>
+            </div>
+          </section>
         </div>
-    )
+      })} */}
+      <div className="container">
+          <section class="card">
+            <img class="card-img-top" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">{projects[0].title}</h5>
+              <p class="card-text">{projects[0].description}</p>
+            </div>
+            <div class="card-body">
+              <a href={`${projects[0].githubLink}`} class="card-link">
+                GitHub Repository
+              </a>
+              <a href={`${projects[0].deployedLink}`} class="card-link">
+                Deployed Site
+              </a>
+            </div>
+          </section>
+        </div>
+    </div>
+  );
 }
