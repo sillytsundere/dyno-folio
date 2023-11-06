df11ee31e2a69c64b35cedfa9d4c11246ad9295e
Paige <paigehcarroll@gmail.com>
2023-11-06 14:38:15 -0600

    added name to header with some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
 import pinImage from "../assets/pin-header.jpeg";
 
-const headerStyle = {
-  width: "100%",
-  height: "300px",
-  backgroundSize: "cover",
-  backgroundImage: `url(${pinImage})`,
-  backgroundRepeat: "no-repeat",
+const styles = {
+  header: {
+    width: "100%",
+    height: "300px",
+    backgroundSize: "cover",
+    backgroundImage: `url(${pinImage})`,
+    backgroundRepeat: "no-repeat",
+  },
+  text: {
+    color: "black",
+    backgroundColor: "rgba(255, 250, 255, 0.751)",
+  },
 };
 
 function Header() {
-  return <div style={headerStyle}></div>;
+  return (
+    <div style={styles.header}>
+      <div className="container text-center" style={{ height: "300px" }}>
+        <div className="row align-items-center justify-content-center" style={{ height: "300px" }}>
+          <h1 className="col-3 rounded" style={styles.text}>
+            Paige
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
