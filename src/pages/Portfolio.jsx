6e5e2422a35b07e7b626ebf97b0c527a110d5d11
Paige <paigehcarroll@gmail.com>
2023-11-06 11:31:20 -0600

    debugged map function on porfolio page, added images to project components

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,52 +1,61 @@
-// function Project({ title, description, githubLink, deployedLink }) {
-//     return (
-//         <div className="container">
-//         <div class="card">
-//           <img class="card-img-top" alt="" />
-//           <div class="card-body">
-//             <h5 class="card-title">{title}</h5>
-//             <p class="card-text">
-//               {description}
-//             </p>
-//           </div>
-//           <div class="card-body">
-//             <a href={`${githubLink}`}class="card-link">
-//               GitHub Repository
-//             </a>
-//             <a href={`${deployedLink}`} class="card-link">
-//               Deployed Site
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     )
-// }
+import bookquesters from '../assets/bookquesters.png';
+import cosfolio from '../assets/cosfolio.png';
+import hangman from '../assets/hangman.png';
+import techTalk from '../assets/comp-sci-blog.png';
+
+function Project({ image, title, description, githubLink, deployedLink }) {
+    return (
+        <div className="container">
+        <div class="card">
+          <img src={image} class="card-img-top" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">
+              {description}
+            </p>
+          </div>
+          <div class="card-body">
+            <a href={githubLink}class="card-link">
+              GitHub Repository
+            </a>
+            <a href={deployedLink} class="card-link">
+              Deployed Site
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+}
 
 export default function Portfolio() {
   const projects = [
     {
-      title: "bookquesters",
+      image: bookquesters,
+      title: "Bookquesters",
       description:
         "This web application allows users to search for book titles sourced from the Google Books API and save them to a list that is saved in local storage.",
       githubLink: "https://github.com/sillytsundere/bookquesters",
       deployedLink: "https://sillytsundere.github.io/bookquesters/",
     },
     {
-      title: "eva - hangman",
+      image: hangman,
+      title: "Eva - Hangman",
       description:
         "A hangman game made using vanilla JavaScript and HTML styled with BootstrapCSS. Themed after a 90's anime.",
       githubLink: "https://github.com/sillytsundere/hangman-game",
       deployedLink: "N/A",
     },
     {
-      title: "cosfolio",
+      image: cosfolio,
+      title: "Cosfolio",
       description:
         "A forum where users can save and share cosplay creations as well as view other users' creations.",
       githubLink: "https://github.com/sillytsundere/cosfolio",
       deployedLink: "https://cosfolio-cbc30e9a9851.herokuapp.com/",
     },
     {
-      title: "comp - sci - blog",
+      image: techTalk,
+      title: "Tech Talk",
       description:
         "This web application is a CMS-style blog that allows users to create an account, log in, share posts and view and comment on other user's posts.",
       githubLink: "https://github.com/sillytsundere/comp-sci-blog",
@@ -57,42 +66,9 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      {/* {projects.map((project) => {
-        <div className="container">
-          <section class="card">
-            <img class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">{project.title}</h5>
-              <p class="card-text">{project.description}</p>
-            </div>
-            <div class="card-body">
-              <a href={`${project.githubLink}`} class="card-link">
-                GitHub Repository
-              </a>
-              <a href={`${project.deployedLink}`} class="card-link">
-                Deployed Site
-              </a>
-            </div>
-          </section>
-        </div>
-      })} */}
-      <div className="container">
-          <section class="card">
-            <img class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">{projects[0].title}</h5>
-              <p class="card-text">{projects[0].description}</p>
-            </div>
-            <div class="card-body">
-              <a href={`${projects[0].githubLink}`} class="card-link">
-                GitHub Repository
-              </a>
-              <a href={`${projects[0].deployedLink}`} class="card-link">
-                Deployed Site
-              </a>
-            </div>
-          </section>
-        </div>
+      {projects.map((project) => (
+        <Project project={project} image={project.image} title={project.title} description={project.description} githubLink={project.githubLink} deployedLink={project.deployedLink}/>
+      ))}
     </div>
   );
 }
