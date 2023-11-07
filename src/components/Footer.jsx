d989dcb35be0460f19fc8575ca353e318632a865
Paige <paigehcarroll@gmail.com>
2023-11-07 14:33:41 -0600

    added hover effects and colors to footer social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { SiInstagram, SiLinkedin, SiGithub } from "react-icons/si";
+import "./Footer.css";
+
+
 
 function Footer() {
+
+  const iconStyle = {
+    instagram: {
+      color: "#d62976",
+    },
+    linkedin: {
+      color: "#0077b5",
+    },
+    github: {
+      color: "#f5f5f5",
+    }
+  }
+
   return (
-    <footer className="d-flex flex-column align-items-center">
+    <footer className="d-flex flex-column align-items-center m-1">
       <section>
-        <a href="https://www.instagram.com/sillytsundere/">
-          <SiInstagram size={32} className="m-2" />
+        <a href="https://www.instagram.com/sillytsundere/" className="icon-link">
+          <SiInstagram size={32} className="m-2 hover-overlay" style={iconStyle.instagram}/>
         </a>
-        <a href="https://www.linkedin.com/in/paigehcarroll2113/">
-          <SiLinkedin size={32} className="m-2" />
+        <a href="https://www.linkedin.com/in/paigehcarroll2113/" className="icon-link">
+          <SiLinkedin size={32} className="m-2" style={iconStyle.linkedin}/>
         </a>
-        <a href="https://github.com/sillytsundere">
-          <SiGithub size={32} className="m-2" />
+        <a href="https://github.com/sillytsundere" className="icon-link">
+          <SiGithub size={32} className="m-2" style={iconStyle.github}/>
         </a>
       </section>
       <p>© 2023 Made by Paige Carroll</p>
