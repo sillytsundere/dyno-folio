3b95ea8a996bc03e78eeb96bfaa4c3fb7620b27f
Paige <paigehcarroll@gmail.com>
2023-12-11 16:34:17 -0600

    added state to footer component to change size of icons with changing screen size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SiInstagram, SiLinkedin, SiGithub } from "react-icons/si";
 import "./Footer.css";
 import { IconContext } from "react-icons";
-
-
 
 function Footer() {
 
@@ -19,11 +17,38 @@ function Footer() {
     }
   }
 
+  const [iconSize, setIconSize] = useState(50);
+
+  useEffect(() => {
+    const updateIconSize = () => {
+      const viewportWidth = window.innerWidth;
+
+      if (viewportWidth <= 600) {
+        setIconSize(32);
+      } else if (viewportWidth <= 800) {
+        setIconSize(50)
+      } else {
+        setIconSize(75)
+      }
+    };
+  
+  // Initial update
+  updateIconSize();
+
+  // Update on window resize
+  window.addEventListener("resize", updateIconSize);
+
+  // Clean up the event listener on component unmount
+  return () => {
+    window.removeEventListener("resize", updateIconSize);
+  };
+ }, []);
+
   return (
     <footer className="d-flex flex-column align-items-center m-1">
       <section>
         <a href="https://www.instagram.com/sillytsundere/" className="m-2 icon-link" style={iconStyle.instagram}>
-        <svg height="75" width="75">
+        <svg height={iconSize} width={iconSize}>
           <defs>
             <linearGradient id="myGradient" gradientTransform="rotate(70)">
               <stop offset="0%" stopColor="#f9ce34" />
@@ -32,18 +57,15 @@ function Footer() {
             </linearGradient>
           </defs>
           <IconContext.Provider value={{ attr: {fill: "url('#myGradient')"}}}>
-            <SiInstagram size={75} className="rounded hover-overlay" />
+            <SiInstagram size={iconSize} className="rounded hover-overlay" />
           </IconContext.Provider>
         </svg>
         </a>
-
-
-
         <a href="https://www.linkedin.com/in/paigehcarroll2113/" className="m-2 rounded icon-link">
-          <SiLinkedin size={75} className="hover-overlay" style={iconStyle.linkedin}/>
+          <SiLinkedin size={iconSize} className="hover-overlay" style={iconStyle.linkedin}/>
         </a>
         <a href="https://github.com/sillytsundere" className="m-2 rounded-circle icon-link">
-          <SiGithub size={75} className="" style={iconStyle.github}/>
+          <SiGithub size={iconSize} className="hover-overlay" style={iconStyle.github}/>
         </a>
       </section>
       <p style={{color: "#32CD32"}}>© 2023 Made by Paige Carroll</p>
