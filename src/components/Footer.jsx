4ae408f8cb9f5edff1a23a472d31bc09bb187ddb
Paige <paigehcarroll@gmail.com>
2023-12-11 15:57:49 -0600

    added color gradient to instagram icon link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SiInstagram, SiLinkedin, SiGithub } from "react-icons/si";
 import "./Footer.css";
+import { IconContext } from "react-icons";
 
 
 
@@ -8,7 +9,7 @@ function Footer() {
 
   const iconStyle = {
     instagram: {
-      color: "#d62976",
+      borderRadius: "25%",
     },
     linkedin: {
       color: "#0077b5",
@@ -21,14 +22,28 @@ function Footer() {
   return (
     <footer className="d-flex flex-column align-items-center m-1">
       <section>
-        <a href="https://www.instagram.com/sillytsundere/" className="icon-link">
-          <SiInstagram size={32} className="m-2 hover-overlay" style={iconStyle.instagram}/>
+        <a href="https://www.instagram.com/sillytsundere/" className="m-2 icon-link" style={iconStyle.instagram}>
+        <svg height="75" width="75">
+          <defs>
+            <linearGradient id="myGradient" gradientTransform="rotate(70)">
+              <stop offset="0%" stopColor="#f9ce34" />
+              <stop offset="75%" stopColor="#ee2a7b" />
+              <stop offset="100%" stopColor="#6228d7" />
+            </linearGradient>
+          </defs>
+          <IconContext.Provider value={{ attr: {fill: "url('#myGradient')"}}}>
+            <SiInstagram size={75} className="rounded hover-overlay" />
+          </IconContext.Provider>
+        </svg>
         </a>
-        <a href="https://www.linkedin.com/in/paigehcarroll2113/" className="icon-link">
-          <SiLinkedin size={32} className="m-2" style={iconStyle.linkedin}/>
+
+
+
+        <a href="https://www.linkedin.com/in/paigehcarroll2113/" className="m-2 rounded icon-link">
+          <SiLinkedin size={75} className="hover-overlay" style={iconStyle.linkedin}/>
         </a>
-        <a href="https://github.com/sillytsundere" className="icon-link">
-          <SiGithub size={32} className="m-2" style={iconStyle.github}/>
+        <a href="https://github.com/sillytsundere" className="m-2 rounded-circle icon-link">
+          <SiGithub size={75} className="" style={iconStyle.github}/>
         </a>
       </section>
       <p style={{color: "#32CD32"}}>© 2023 Made by Paige Carroll</p>
