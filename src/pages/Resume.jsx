d282b47eaa5a2e2c96b1feaec48ac01e8af4403a
Paige <paigehcarroll@gmail.com>
2023-12-12 18:23:39 -0600

    edited resume page styling, and changed link to goog doc res

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,23 +1,18 @@
 import "./Resume.css";
 
 export default function Resume() {
-
   return (
-    <div>
-      <h1 className="m-2" style={{color: "#D0F0C0"}}>Resume</h1>
-      <div className="d-flex justify-content-center">
-        <div className="card col-9 text-center">
+    <div className="one">
+      <h1 className="resume-title">
+        Resume
+      </h1>
+      <div>
+        <div className="card text-center">
           <div className="card-body">
-            <h5 className="card-title">My Technical Resume</h5>
+            <h5 className="card-title"><a href="https://docs.google.com/document/d/10jcyc6s6PcLadUw4XbPQO2zV3YXp79jzITxEisokdeU/edit?usp=sharing" className="card-link link">Technical Resume</a></h5>
             <p className="card-text">
               Skills Include: HTML, CSS, JavaScript, React, Node, Express, Git, MySQL, MongoDB, Apollo, GraphQL and more!
             </p>
-            <a
-              href="https://docs.google.com/document/d/10jcyc6s6PcLadUw4XbPQO2zV3YXp79jzITxEisokdeU/edit?usp=sharing"
-              className="card-link link"
-            >
-              Google Docs Resume
-            </a>
           </div>
         </div>
       </div>
