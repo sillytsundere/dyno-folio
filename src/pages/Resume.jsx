fda058d6ced8cfe9f5541b42d267c699288d82db
Paige <paigehcarroll@gmail.com>
2024-04-09 16:14:38 -0500

    Resume component now just served pdf view of embeded resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,29 +1,12 @@
-import "./Resume.css";
-
 export default function Resume() {
   return (
-    <div className="one">
-      <h1 className="resume-title shadows-into-light-regular">Resume</h1>
       <div>
-        <div className="card text-center">
-          <div className="card-body quicksand-font">
-            <h5 className="card-title">
-              <a
-                href="https://docs.google.com/document/d/1PtlxedYoIhz1y00ahsI6c8QKtW9FpetFWf6TEZGKLHo/edit?usp=sharing"
-                className="card-link link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Technical Resume
-              </a>
-            </h5>
-            <p className="card-text">
-              Skills Include: HTML, CSS, JavaScript, React, Node, Express, Git,
-              MySQL, MongoDB, Apollo, GraphQL and more!
-            </p>
-          </div>
-        </div>
+          <embed 
+          src="src/assets/Paige-H-Carroll-Resume.pdf" 
+          type="application/pdf" 
+          style={{width: "100%", height: "100vh"}}
+          >
+          </embed>
       </div>
-    </div>
   );
 }
