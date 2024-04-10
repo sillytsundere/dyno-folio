babbb3b8d567ae698b38ff2f1b0ed0f7f8a9943f
Paige <paigehcarroll@gmail.com>
2024-04-10 17:30:04 -0500

    installed vite-plugin-copy to serve pdf from appropriate location to work in production env

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,11 @@
 import "./Resume.css";
+import paigeHCarrollResume from "../assets/Paige-H-Carroll-Resume.pdf"
 
 export default function Resume() {
   return (
       <div>
           <embed 
-          src="src/assets/Paige-H-Carroll-Resume.pdf" 
+          src={paigeHCarrollResume} 
           type="application/pdf" 
           style={{width: "100%", height: "100vh"}}
           >
