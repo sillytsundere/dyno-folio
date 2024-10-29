eb383513c89f24c0efabbb4c607ca6d558cd0183
Paige <paigehcarroll@gmail.com>
2024-10-28 20:39:41 -0500

    updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,12 @@
 import "./Resume.css";
 import paigeHCarrollResume from "../assets/Paige-H-Carroll-Resume.pdf"
+import novTwentyFourResume from "../assets/Paige_Carroll_Resume.pdf"
 
 export default function Resume() {
   return (
       <div>
           <embed 
-          src={paigeHCarrollResume} 
+          src={novTwentyFourResume} 
           type="application/pdf" 
           style={{width: "100%", height: "100vh"}}
           >
